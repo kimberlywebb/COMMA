440e574470972288a5f0eeff60e75c0be47f8286
Kim Hochstedler Webb <kimhochstedler@gmail.com>
2024-09-26 10:59:59 -0400

    update NCHS data

--- data-raw/NCHS2022_sample.R
+++ data-raw/NCHS2022_sample.R
@@ -1,6 +1,6 @@
 ## code to prepare `NCHS2022_sample` dataset goes here
 
-save_wd <- "C:/Users/KIW58/OneDrive/Documents/GitHub/COMMA/data_raw/"
+save_wd <- "C:/Users/KIW58/OneDrive/Documents/GitHub/COMMA/data-raw/"
 NCHS2022_sample <- read.csv(paste0(save_wd, "Nat2022US_sample_COMMA.csv"))
 
 usethis::use_data(NCHS2022_sample, overwrite = TRUE)
